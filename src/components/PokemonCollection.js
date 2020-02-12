dff4e71f6f8fea41287047eae61d69f4bdc3a813
Miss-Cheese <nat.galligan@gmail.com>
2020-02-11 21:53:23 -0500

    Done

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,15 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
   render() {
+
+    let displayedPokemons = this.props.pokemons
+    displayedPokemons = displayedPokemons.filter(pokemon => pokemon.name.includes(this.props.searchInput))
+
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {displayedPokemons.map(pokemon => <PokemonCard {...pokemon} key={pokemon.id} />)}
       </Card.Group>
     )
   }
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,94 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemons: [],
+    searchInput: ""
+  }
+
+  componentDidMount () {
+    fetch("http://localhost:3000/pokemon")
+    .then(response => response.json())
+    .then(pokemonData => this.setState({
+      pokemons: pokemonData
+    }))
+  }
+
+  filterPokemons = (event) => {
+    this.setState({
+      searchInput: event.target.value
+    })
+  }
+
+  finishSubmit = (newPokemon) => {
+
+    let sentPokemon = 
+    {
+      "height": null,
+      "weight": null,
+      "id": null,
+      "name": newPokemon.name,
+      "abilities": [null],
+      "moves": [],
+      "stats": [
+        {
+          "value": null,
+          "name": "special-defense"
+        },
+        {
+          "value": null,
+          "name": "special-attack"
+        },
+        {
+          "value": null,
+          "name": "defense"
+        },
+        {
+          "value": null,
+          "name": "attack"
+        },
+        {
+          "value": null,
+          "name": "speed"
+        },
+        {
+          "value": newPokemon.hp,
+          "name": "hp"
+        }
+      ],
+      "types": [null],
+      "sprites": {
+        "front": newPokemon.frontUrl,
+        "back": newPokemon.backUrl
+      }
+    }
+
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(sentPokemon)
+    })
+    .then(response => response.json())
+    .then(pokemonData => this.setState({
+      pokemons: [...this.state.pokemons, pokemonData]
+    }))
+  }
+
   render() {
+
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm finishSubmit={this.finishSubmit}/>
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search filterPokemons={this.filterPokemons} searchInput={this.state.searchInput} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.state.pokemons} searchInput={this.state.searchInput}/>
       </Container>
     )
   }
